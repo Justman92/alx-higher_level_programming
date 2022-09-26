d55e38d73b4a7ac4214f51b24ed55d046617c182
Justice Anthony <justiceanthony690@gmail.com>
2022-09-26 04:42:38 -0700

    print a integer of list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """print integers in a list
+    Args:
+        my_list (list, optional): list of integers. Defaults to [].
+    Returns: None
+    """
+    for item in my_list:
+        print("{:d}".format(item))
